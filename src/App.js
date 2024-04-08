674cb1af4c75002d7e000a822f6296cab1e5a3c6
50me0ne <irvin.garcia@outlook.com>
2024-04-08 13:09:51 -0600

    load-more-data

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Accordian from './components/accordion';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating'
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 
 function App() {
   return (
@@ -16,7 +17,9 @@ function App() {
       {/*<StarRating noOfStars={10}/>*/}
       {/*Star rating component */}
       {/*Image slider component */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
+      {/*<ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>*/}
+      {/*Load More Products component */}
+      <LoadMoreData/>
       {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
